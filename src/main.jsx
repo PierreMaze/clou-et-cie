28eb3e254dc39700f7edeaac1ae2fbdc2cc30e4c
Pierre M. <promazelaygue@gmail.com>
2024-12-13 04:33:23 +0100

    Renamed file Landing.jsx to Hero.jsx and removed unused files. (bug)

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import HeroSection from './pages/Hero.jsx';
-import CGU from './pages/CGU.jsx';
-import MentionsLegales from './pages/MentionsLegales.jsx';
 import Header from './layout/Header';
+import HeroSection from './pages/Hero.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
     <HeroSection />
-    <CGU />
-    <MentionsLegales />
   </StrictMode>
 );
