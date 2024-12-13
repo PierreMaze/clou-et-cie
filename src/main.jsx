71bf613e1d78dec1ab9f85e084eb3fa7e89ddca3
Pierre M. <promazelaygue@gmail.com>
2024-12-13 04:17:04 +0100

    Renamed file Home.jsx to Landing.jsx for better clarity.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import Home from './pages/Home.jsx';
+import Landing from './pages/Home.jsx';
 import CGU from './pages/CGU.jsx';
 import MentionsLegales from './pages/MentionsLegales.jsx';
 import Header from './layout/Header';
@@ -9,7 +9,7 @@ import Header from './layout/Header';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
-    <Home />
+    <Landing />
     <CGU />
     <MentionsLegales />
   </StrictMode>
