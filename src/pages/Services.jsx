84650a5e600c518cbcf64f5ab947f53c083e2b22
Pierre M. <promazelaygue@gmail.com>
2024-12-13 17:08:21 +0100

    Updated the Services table and modified the map to display an unordered list.

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -6,136 +6,107 @@ import {
   FaHammer,
 } from '../utils/FontAwesome.config.jsx';
 
-const detailsAboutTheServices = [
+const services = [
   {
     id: 1,
-    name: 'Salle de Bain.',
-    description: (
-      <>
-        Transformez votre salle de bain en un espace fonctionnel et moderne :
-        <ul className="list-disc">
-          <li>Rénovation complète ou partielle.</li>
-          <li>Installation sanitaire.</li>
-          <li>Pose de carrelage et finitions soignées.</li>
-        </ul>
-      </>
-    ),
-
+    title:
+      '<strong>Salle de Bain.</strong> Transformez votre salle de bain en un espace fonctionnel et moderne :',
+    list: [
+      'Rénovation complète ou partielle.',
+      'Installation sanitaire.',
+      'Pose de carrelage.',
+    ],
     icon: FaSink,
   },
   {
     id: 2,
-    name: 'Cuisine.',
-    description: (
-      <>
-        Aménagez une cuisine à la hauteur de vos attentes :
-        <ul className="list-disc">
-          <li>Installation et rénovation de meubles.</li>
-          <li>Pose de carrelage.</li>
-          <li>Solutions sur mesure pour optimiser l’espace et l’esthétique.</li>
-        </ul>
-      </>
-    ),
-
+    title:
+      '<strong>Cuisine.</strong> Aménagez une cuisine à la hauteur de vos attentes :',
+    list: [
+      'Installation et rénovation de meubles.',
+      'Pose de carrelage.',
+      "Solution pour optimiser l'espace.",
+    ],
     icon: FaKitchenSet,
   },
   {
     id: 3,
-    name: 'Plâtrerie.',
-    description: (
-      <>
-        Améliorez vos espaces intérieurs avec des travaux de plâtrerie de
-        qualité :
-        <ul className="list-disc">
-          <li>Pose de cloisons pour créer de nouveaux espaces.</li>
-          <li>Réalisation d’enduits pour des finitions parfaites.</li>
-          <li>Travaux d’isolation et décoration intérieure.</li>
-        </ul>
-      </>
-    ),
-
+    title:
+      '<strong>Plâtrerie.</strong> Améliorez vos espaces intérieurs avec des travaux de plâtrerie de qualité :',
+    list: [
+      'Pose de cloisons pour créer de nouveaux espaces.',
+      "Réalisation d'enduits pour des finitions parfaites.",
+      'Travaux d’isolation thermique et phonique.',
+    ],
     icon: FaTrowel,
   },
   {
     id: 4,
-    name: 'Petits Travaux et Réparations.',
-    description: (
-      <>
-        Pour vos besoins ponctuels, je propose des interventions rapides et
-        fiables :
-        <ul className="list-disc">
-          <li>Réparations mineures.</li>
-          <li>Ajustements divers pour entretenir votre habitat.</li>
-          <li>Assistance pour vos petits travaux du quotidien.</li>
-        </ul>
-      </>
-    ),
-
+    title:
+      '<strong>Petits Travaux et Réparations.</strong> Pour vos besoins ponctuels, je propose des interventions rapides et fiables :',
+    list: [
+      'Réparations mineures.',
+      'Ajustements divers pour entretenir votre habitat.',
+      'Assistance pour vos petits travaux du quotidien.',
+    ],
     icon: FaHammer,
   },
 ];
 
 const Services = () => {
   return (
-    <div className="h-screen">
-      <div className="py-12 overflow-hidden sm:py-24">
-        <div className="px-6 mx-auto max-w-7xl md:px-8 lg:px-10 xl:px-14 2xl:px-18">
-          <div className="grid max-w-2xl grid-cols-1 mx-auto gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-            <div className="lg:pr-8 lg:pt-4 2xl:-ml-32 2xl:mr-19 min-[1900px]:-ml-64 min-[1900px]:mr-40 min-[2000px]:-ml-80 min-[2000px]:mr-30">
-              <div className=" lg:max-w-lg 2xl:max-w-full">
-                {/* <!--Header section--> */}
-                <div>
-                  <h2 className="text-lg font-semibold leading-7 text-orange-800 uppercase">
-                    Des services complets
-                  </h2>
-                  <p className="mt-2 text-3xl font-bold tracking-tight uppercase text-stone-900 font-kreon sm:text-4xl">
-                    pour tous vos travaux
-                  </p>
-                  <p className="mt-6 font-medium leading-8 lg:mr-6 text-stone-800 text-md">
-                    Que ce soit pour une rénovation, des travaux de plomberie ou
-                    des aménagements intérieurs, je vous accompagne à chaque
-                    étape, avec des solutions sur mesure et adaptées à vos
-                    besoins.
-                  </p>
-                  <p className="mt-6 font-medium leading-8 lg:mr-6 text-stone-800 text-md">
-                    Grâce à mon expérience, je vous garanti des résultats
-                    durables et de qualité.
-                  </p>
-                </div>
-                {/* <!--Services offered sections--> */}
-                <dl className="max-w-xl mt-10 space-y-8 text-base leading-7 text-stone-800 lg:max-w-none">
-                  {detailsAboutTheServices.map((details) => (
-                    <div
-                      key={details.id}
-                      className="relative ml-12 mr-2 lg:mr-0 lg:ml-9"
-                    >
-                      <div className="inline mr-1 font-semibold text-stone-900">
-                        <span className="mr-4 -ml-10">
-                          <details.icon
-                            aria-hidden="true"
-                            className="absolute text-orange-600 size-5 left-1 top-1 "
-                          />
-                        </span>
-                        {details.name}
-                      </div>
-                      <div className="inline">{details.description}</div>
-                    </div>
-                  ))}
-                </dl>
-                {/* <!--Action button--> */}
-                <button
-                  onClick="window.location.href='#contact'"
-                  className="w-full py-4 mx-auto mt-10 text-sm font-semibold leading-7 duration-300 ease-in-out delay-100 bg-orange-800 rounded-lg shadow-sm text-orange-50 lg:max-w-none md:w-fit md:px-8 hover:bg-orange-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-800"
-                >
-                  Obtener votre devis gratuit
-                </button>
-              </div>
+    <div className="py-24 overflow-hidden bg-orange-50 sm:py-32">
+      <div className="px-6 mx-auto max-w-7xl lg:px-8">
+        <div className="grid max-w-2xl grid-cols-1 mx-auto gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+          <div className="lg:pt-4 lg:pr-8">
+            <div className="lg:max-w-lg">
+              <h2 className="font-semibold text-orange-800 text-base/7">
+                Des services complets
+              </h2>
+              <p className="mt-2 text-4xl font-semibold tracking-tight text-stone-900 text-pretty sm:text-5xl">
+                pour tous vos travaux.
+              </p>
+              <p className="mt-6 text-stone-600 text-lg/8">
+                Que ce soit pour une rénovation, des travaux de plomberie ou des
+                aménagements intérieurs, je vous accompagne à chaque étape, avec
+                des solutions adaptées à vos besoins.
+              </p>
+              <dl className="max-w-xl mt-10 space-y-8 text-stone-600 text-base/7 lg:max-w-none">
+                {services.map((service) => (
+                  <div key={service.id} className="relative ml-4">
+                    <dt className="flex items-start space-x-1 font-semibold -ml-2s text-stone-900">
+                      <service.icon
+                        aria-hidden="true"
+                        className="text-orange-800 size-5"
+                      />
+                      <span>{service.title}</span>
+                      &nbsp;{service.description}
+                    </dt>
+                    <ul className="pl-10 mt-2 list-disc text-stone-700">
+                      {service.list.map((element, index) => (
+                        <li key={index}>{element}</li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+              </dl>
             </div>
-            {/* <!--Picture Renovation Container--> */}
+            <div className="flex">
+              <a
+                href="#contact"
+                className="w-full py-4 mt-10 text-sm font-semibold leading-7 text-center duration-300 ease-in-out delay-100 bg-orange-800 rounded-lg shadow-sm lg:ml-6 mx- text-orange-50 sm:max-w-none sm:w-fit sm:px-8 hover:bg-orange-900"
+              >
+                Obtener votre devis gratuit
+              </a>
+            </div>
+          </div>
+          <div className="relative">
             <img
-              alt="Rooftop(= Terasse sur le toit) de luminescence."
+              alt="Product screenshot"
               src={RenovationPicture}
-              className="w-[24rem] max-w-full rounded-xl shadow-xl ring-1 ring-stone-400/10 lg:max-w-none sm:w-[50rem] md:-ml-4 lg:-ml-20 min-[2000px]:w-[90rem] "
+              width={2432}
+              height={1442}
+              className="sticky top-10 w-full max-w-none rounded-xl ring-1 shadow-xl ring-stone-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
             />
           </div>
         </div>
