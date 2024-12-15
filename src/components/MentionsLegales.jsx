2642ac9f965e05f1b7c6b25580a0435f3831f24d
Pierre M. <promazelaygue@gmail.com>
2024-12-15 09:17:29 +0100

    Created a modal component for legal mentions with toggle functionality, links, and company information.

---
+++ src/components/MentionsLegales.jsx
@@ -0,0 +1,118 @@
+import PropTypes from 'prop-types';
+import { FaCircleXmark } from 'react-icons/fa6';
+
+const LegalMentions = ({ isOpen, onClose }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-opacity-50 bg-stone-800">
+      <div className="relative bg-orange-50 rounded-lg shadow-xl w-full max-w-4xl max-h-[90vh] lg:max-h-[80vh] overflow-hidden">
+        {/* Close Icon */}
+        <button
+          onClick={onClose}
+          className="absolute z-10 text-stone-500 top-4 right-4 hover:text-stone-700"
+        >
+          <FaCircleXmark size={24} className="text-orange-700" />
+        </button>
+
+        {/* Modal Title */}
+        <div className="p-6 pb-2 border-b">
+          <h2 className="text-2xl font-semibold text-orange-700">
+            Mentions Légales
+          </h2>
+        </div>
+
+        {/* Scrollable Content */}
+        <div className="p-6 overflow-y-auto max-h-[60vh]">
+          <section className="mb-6">
+            <h3 className="mb-4 text-xl font-medium">
+              1. Identification de l&apos;entreprise
+            </h3>
+            <p className="mb-2 text-stone-600">
+              <strong>Raison sociale :</strong> Clou & Cie
+            </p>
+            <p className="mb-2 text-stone-600">
+              <strong>Forme juridique :</strong> Micro entreprise
+            </p>
+            <p className="mb-2 text-stone-600">
+              <strong>Adresse :</strong> 85 Rue de Lacanau, 33200 Bordeaux,
+              FRANCE
+            </p>
+            <p className="mb-2 text-stone-600">
+              <strong>Numéro de SIRET :</strong> 53790427800026
+            </p>
+          </section>
+
+          <section className="mb-6">
+            <h3 className="mb-4 text-xl font-medium">
+              2. Directeur de publication
+            </h3>
+            <p className="text-stone-600">
+              <strong>Pierre Mathieu MAZELAYGUE - </strong>
+              <a
+                href="mailto:promazelaygue@gmail.com"
+                className="text-orange-700 hover:underline hover:underline-offset-4"
+              >
+                promazelaygue@gmail.com
+              </a>
+            </p>
+          </section>
+
+          <section className="mb-6">
+            <h3 className="mb-4 text-xl font-medium">3. Hébergement</h3>
+            <p className="mb-2 text-stone-600">
+              <strong>Nom de l&apos;hébergeur :</strong> Netlify, Inc
+            </p>
+            <p className="text-stone-600">
+              <strong>Coordonnées :</strong> Netlify, Inc. 512, 2e rue, Fl 2 San
+              Francisco, CA 94107
+            </p>
+          </section>
+
+          <section className="mb-6">
+            <h3 className="mb-4 text-xl font-medium">
+              4. Propriété intellectuelle
+            </h3>
+            <p className="text-stone-600">
+              L&apos;ensemble des contenus présents sur ce site (textes, images,
+              graphismes, logo, etc.) est la propriété exclusive de Clou & Cie,
+              sauf mention contraire. Toute reproduction, distribution,
+              modification, adaptation, retransmission ou publication, même
+              partielle, est strictement interdite sans l&apos;accord exprès de
+              Clou & Cie.
+            </p>
+          </section>
+
+          <section className="mb-6">
+            <h3 className="mb-4 text-xl font-medium">5. Cookies</h3>
+            <p className="text-stone-600">
+              Notre site utilise des cookies pour améliorer votre expérience de
+              navigation. Vous pouvez paramétrer vos préférences concernant les
+              cookies à tout moment.
+            </p>
+          </section>
+
+          <section>
+            <h3 className="mb-4 text-xl font-medium">6. Contact</h3>
+            <p className="mb-2 text-stone-600">
+              <strong>Email :</strong> patrick-mazelaygue@gmail.com
+            </p>
+            <p className="text-stone-600">
+              <strong>Téléphone :</strong>{' '}
+              <span className="tabular-nums slashed-zero">
+                +33 06.31.90.95.35
+              </span>
+            </p>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+LegalMentions.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default LegalMentions;
--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,26 +1,43 @@
+import { useState } from 'react';
 import LogoClouEtCie from '/images/logo-clou-et-cie.webp';
+import LegalMentions from '../components/MentionsLegales.jsx';
+
 const Footer = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
   return (
-    <footer className="h-full shadow bg-stone-800">
-      <div className="w-full max-w-screen-xl p-4 mx-auto md:flex md:items-center md:justify-between">
-        <a href="#" className="-m-1.5 p-1.5">
-          <span className="sr-only">Clou & Cie</span>
-          <div className="flex items-center">
-            <img className="w-auto h-8" src={LogoClouEtCie} alt="" />
-            <div className="ml-4">
-              <h1 className="text-lg font-bold text-orange-50">Clou & Cie</h1>
-              <p className="text-sm tracking-wide text-stone-500">
-                Le clou qui fait tout.
-              </p>
+    <>
+      <footer className="h-full shadow bg-stone-800">
+        <div className="flex flex-col items-center w-full max-w-screen-xl gap-5 p-4 mx-auto lg:gap-0 lg:flex-row md:flex md:items-center md:justify-between">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Clou & Cie</span>
+            <div className="flex items-center">
+              <img className="w-auto h-8" src={LogoClouEtCie} alt="" />
+              <div className="ml-4">
+                <h1 className="text-lg font-bold text-orange-50">Clou & Cie</h1>
+                <p className="text-sm tracking-wide text-stone-500">
+                  Le clou qui fait tout.
+                </p>
+              </div>
             </div>
-          </div>
-        </a>
-        <span className="text-sm text-stone-400 sm:text-center ">
-          © 2024 Clou&Cie. All Rights Reserved.
-        </span>
-        <button className="text-orange-50">Mentions Legales</button>{' '}
-      </div>
-    </footer>
+          </a>
+          <button
+            onClick={openModal}
+            className="text-orange-50 hover:underline"
+          >
+            Mentions Legales
+          </button>
+          <span className="text-sm text-stone-400 sm:text-center ">
+            © 2024 Clou&Cie. All Rights Reserved.
+          </span>
+        </div>
+      </footer>
+
+      <LegalMentions isOpen={isModalOpen} onClose={closeModal} />
+    </>
   );
 };
 
