c0e084b44297921a4a161bb9940fe3324ce486de
Pierre M. <promazelaygue@gmail.com>
2024-12-18 08:36:05 +0100

    Logo size updated

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Clou & Cie</span>
             <div className="flex items-center">
-              <img className="w-auto h-8" src={LogoClouEtCie} alt="" />
+              <img className="w-auto h-10" src={LogoClouEtCie} alt="" />
               <div className="ml-4">
                 <h1 className="text-lg font-bold text-orange-50">Clou & Cie</h1>
                 <p className="text-sm tracking-wide text-stone-500">
